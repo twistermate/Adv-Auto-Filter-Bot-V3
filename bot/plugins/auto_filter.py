83b070c6f2c376ccc873ec2d6d0761536865211f
Professor <81632023+wafikhsalman@users.noreply.github.com>
2021-05-30 18:12:46 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -158,13 +158,13 @@ async def auto_filter(bot, update):
         if len_result != 1:
             result[0].append(
                 [
-                    InlineKeyboardButton("Next ⏩", callback_data=f"navigate(0|next|{query})")
+                    InlineKeyboardButton("𝐍𝐄𝐗𝐓 ⏩", callback_data=f"navigate(0|next|{query})")
                 ]
             )
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f"🔰 Page 1/{len_result if len_result < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f"🗒 𝐏𝐀𝐆𝐄 1/{len_result if len_result < max_pages else max_pages} ", callback_data="ignore")
         ])
         
         
