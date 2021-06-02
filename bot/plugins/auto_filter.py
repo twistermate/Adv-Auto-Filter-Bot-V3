8c9130d0927ab1f04d02731dad05cefebcab2545
Professor <81632023+wafikhsalman@users.noreply.github.com>
2021-06-02 16:43:53 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -74,7 +74,7 @@ async def auto_filter(bot, update):
             # from B to MiB
             
             if file_size < 1024:
-                file_size = f"[{file_size} B]"
+                file_size = f"{file_size} B"
             elif file_size < (1024**2):
                 file_size = f"[{str(round(file_size/1024, 2))} KiB] "
             elif file_size < (1024**3):
