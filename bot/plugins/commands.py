6768cf677cc8e1bebc07afa1433a486d78809f10
Professor <81632023+wafikhsalman@users.noreply.github.com>
2021-06-12 14:38:49 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG & MRK-YT
+# (c) @AlbertEinsteinTG & Wafikh
 
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
