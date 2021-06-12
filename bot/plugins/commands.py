b8b0257fd5b6419e2a9f4dfeeed14b2b929641bf
Professor <81632023+wafikhsalman@users.noreply.github.com>
2021-06-12 14:39:20 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -119,7 +119,7 @@ async def start(bot, update):
     
     await bot.send_photo(
         chat_id=update.chat.id,
-        photo="https://telegra.ph/file/30887a2245e0f405861bc.jpg",
+        photo="https://telegra.ph/file/6b620720209c4a8761c95.jpg",
         caption=Translation.START_TEXT.format(
                 update.from_user.first_name),
         reply_markup=reply_markup,
