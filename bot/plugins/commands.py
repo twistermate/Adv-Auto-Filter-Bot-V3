8bed154a7bc9afa79f6a5a37d1904326001a7659
jokermon <85845434+jokermon@users.noreply.github.com>
2021-08-02 20:01:51 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -12,7 +12,7 @@ db = Database()
 
 @Client.on_message(filters.command(["start"]) & filters.private, group=1)
 async def start(bot, update):
-    update_channel = "@ch_hindi"
+    update_channel = "@kuruthi_amzonprime"
     if update_channel:
         try:
             user = await bot.get_chat_member(update_channel, update.chat.id)
@@ -24,7 +24,7 @@ async def start(bot, update):
             await update.reply_text(
                 text="<b>🔊 𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗠𝗮𝗶𝗻 𝗰𝗵𝗮𝗻𝗻𝗲𝗹 🤭\n\nനിങ്ങൾക് സിനിമകൾ വെന്നോ? അതിനായി അത്യം ങ്ങളുടെ മെയിൻ ചാനലിൽ ജോയിൻ ചെയ്യണം... 😁\n\nJoin ചെയതത്തിനു ശേഷം വീണ്ടും ബോട്ട് /start ആക്കൂ.😁</b>",
                 reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text=" 🔰JOIN OUR CHANNEL🔰 ", url=f"https://t.me/ch_hindi")]
+                    [ InlineKeyboardButton(text=" 🔰JOIN OUR CHANNEL🔰 ", url=f"https://t.me/kuruthi_amzonprime")]
               ])
             )
             return
@@ -107,7 +107,7 @@ async def start(bot, update):
 
     buttons = [[
         InlineKeyboardButton('⚠️ JOIN', url='https://t.me/joinchat/s3MC2Q0hHVw0Y2U9'),
-        InlineKeyboardButton('🕵‍♂ CREATOR', url ='https://t.me/Wafikh')
+        InlineKeyboardButton('🕵‍♂ CREATOR', url ='https://t.me/cinehut')
     ],[
         InlineKeyboardButton('💡 HELP', callback_data="help"),
         InlineKeyboardButton('🔐 CLOSE', callback_data="close")
